ee6aa55d5005b6b34253236781855fcdc0b090ad
Gary Tokman <gary.tokman@gotruemotion.com>
2020-01-26 14:05:14 -0500

    Initial Commit

---
+++ FoodHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodHub
+//
+//  Created by Gary Tokman on 1/26/20.
+//  Copyright © 2020 Gary Tokman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
